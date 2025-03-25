5d89ee25edc74b37b8cd41e7e1d518f7a1c17f56
mark <xu-jie.duan@hp.com>
2025-03-25 22:12:02 +0800

    1

--- frontend/src/api/chatApi.js
+++ frontend/src/api/chatApi.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // 创建axios实例
 const api = axios.create({
-  baseURL: 'http://backend:5001/api',
+  baseURL: 'api',
+  // baseURL: 'http://39.107.159.184:5001/api',
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json'
