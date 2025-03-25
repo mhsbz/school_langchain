69ff7864b66a642a53b7ffa3fb44a84b9541f32c
mark <xu-jie.duan@hp.com>
2025-03-25 16:45:15 +0800

    add localhost

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 创建axios实例
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: '/api',
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json'
